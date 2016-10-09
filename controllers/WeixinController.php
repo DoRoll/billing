23d8498739a88ad2d100862694403fe7fc3a7647
DoRoll <819698405@qq.com>
2016-10-09 23:27:15 +0800

    完成微信token验证

--- controllers/WeixinController.php
+++ controllers/WeixinController.php
@@ -12,12 +12,16 @@ class WeixinController extends Controller
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
-        $token = Yii::$app->params['weixin_token'];
+        $token = \Yii::$app->params['weixin_token'];
         $tmpArr = array($token, $timestamp, $nonce);
         
         sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
+        $tmpStr = implode( "", $tmpArr );
         $tmpStr = sha1( $tmpStr );
+
+        $objFile = fopen("a.txt", "a");
+        fwrite($objFile, $tmpStr."\r\n-------".$signature);
+        fclose($objFile);
         
         if( $tmpStr == $signature ){
             echo $_GET["echostr"];exit();
