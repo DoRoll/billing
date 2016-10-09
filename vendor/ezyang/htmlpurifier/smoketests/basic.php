fb637e636671a27cbf5b40e0631bddf9fc3a24f9
DoRoll <819698405@qq.com>
2016-10-09 22:29:42 +0800

    删除部分文件

--- vendor/ezyang/htmlpurifier/smoketests/basic.php
+++ vendor/ezyang/htmlpurifier/smoketests/basic.php
@@ -20,10 +20,6 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
 <!DOCTYPE html
      PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
      "http://www.w3.org/TR/xhtml1/DTD/xhtml1- Strict.dtd">
-<?php } else { ?>
-<!DOCTYPE html
-     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-loose.dtd">
 <?php } ?>
 <html>
 <head>
